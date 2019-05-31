ebf796fab8e0b694fecb4a2431991432645fa25e
BoykoDenis <denya.boyko@gmail.com>
2019-05-31 16:16:23 +0200

    First commit of tic-tac-toe game. Start making of AI for it

--- grphic.py
+++
@@ -1,8 +0,0 @@
-from tkinter import *
-from control import *
-from dispset import *
-#def grid(display_width, display_hight, nomberOfboxes, cell_size, field):
-root = Tk()	
-DisplaySetup()
-CreateField(can, field, nomberOfboxes, cell_size)
-mainloop()
