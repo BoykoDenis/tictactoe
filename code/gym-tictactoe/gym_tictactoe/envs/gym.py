e12e23cfe90b261b0aa4adee202bac50641f7794
BoykoDenis <denya.boyko@gmail.com>
2019-07-04 11:29:12 +0200

    Add files via upload

---
+++ code/gym-tictactoe/gym_tictactoe/envs/gym.py
@@ -0,0 +1,24 @@
+from tictactoe_env import TicTacToeEnv as MEnv #main env
+import random
+import tkinter as tk
+import sys
+
+env = MEnv(19, 650, 650)
+root = env.render()
+for epoch in range (0, 2):
+	env.reset()
+	for ev in range (0, 361):
+		print("epoch: ", epoch)
+		print("evo-step: ", ev)
+		action = random.choice(random.choice(env.action_space))
+		if action == None:
+			while action is None:
+				action = random.choice(random.choice(env.action_space))
+				#print("action is: ", action)
+		a, b = action		
+		env.action_space[a][b] = None
+		observation, reword, done, info = env.step(action)
+		print("step output: ", observation, done, reword, info)
+		if done == True:
+			print("done")
+		root.update()
